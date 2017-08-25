b5abe0617973e44627bb7464f18b224b6998aafb
Yinthuu <yinthuaye92>
2017-08-25 15:44:19 +0800

    First commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { MyCompComponent } from './components/my-comp.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MyCompComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
